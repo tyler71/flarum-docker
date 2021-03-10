cbdbe76f9f756e909af700616387c9343d7b3a87
Tyler <4926565+tyler71@users.noreply.github.com>
2021-03-10 12:45:15 -0800

    remove plugin specific
    refactor a little

--- docker/forum_config/forum_setup.sh
+++ docker/forum_config/forum_setup.sh
@@ -12,10 +12,7 @@ mkdir -p \
 if [ -f /conf/config.php ]; then
   ln -fs /conf/config.php ./config.php
   php flarum migrate
-  # Requires Cache Assets
-  # https://discuss.flarum.org/d/23321-cache-assets-by-bokt
   php flarum cache:clear
-  php flarum cache:assets --js --css --locales || true
 
 elif [ "$INSTALL" == "true" ]; then
   cat > install.yml <<EOF
@@ -36,8 +33,9 @@ adminUser:
 settings:
 EOF
   php flarum install --file install.yml
-  cp config.php /conf/config.php
+  mv config.php /conf/config.php
   chown "${PUID_ID}" /conf/config.php
+  ln -fs /conf/config.php ./config.php
 fi
 
 chown "${PUID_ID}" -R  \
